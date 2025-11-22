a05ab9691099bb5826593124cfc3d9b0997f6561
manav111111 <mg769842@gmail.com>
2025-11-22 22:52:44 +0530

    updated home

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,4 +1,4 @@
-// sections/Home.jsx
+import react from 'react';
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { ChevronDown, Github, Linkedin, Twitter, Mail, ArrowRight } from 'lucide-react';
